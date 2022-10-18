6b8eb3a6355f2563d4d3e70b297bf4d306928946
Oleksandr Kuchmuk <96177236+OleksandrKuchmuk@users.noreply.github.com>
2022-10-18 13:37:58 +0300

    Deleted extra lines from the CSV test

--- src/test/java/com/cursor/CSVTest.java
+++ src/test/java/com/cursor/CSVTest.java
@@ -8,18 +8,13 @@ import java.util.Iterator;
 
 public class CSVTest {
 
-
-
     @DataProvider(name = "dpWithCsv")
-    public Iterator<Object[]> csvDataProvider(){
-//        return new CSVReader.readFromCSV();
-//        return new CSVReaderMy().ourDataSet();
-//        return new CSVReaderMy().readFromCSV2();
-   return new MyCSVReader().readFromCsv();
+    public Iterator<Object[]> csvDataProvider() {
+        return new MyCSVReader().readFromCsv();
     }
 
     @Test(dataProvider = "dpWithCsv")
-    public void testWithCsv(String valueA, String valueB){
+    public void testWithCsv(String valueA, String valueB) {
         Assert.assertTrue(valueA.contains(valueB));
     }
 }
